19d60636841614a6a00037e52be09f0e2c36ce26
Matt Harasymczuk <matt@harasymczuk.pl>
2016-12-31 18:48:58 +0100

    Unregister admin for experiments

--- backend/experiment/admin.py
+++ backend/experiment/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from backend.experiment.models import Experiment
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 @admin.register(Experiment)
